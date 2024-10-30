9cb5baff4fe41089cf0bff24157c7557aaf4a2fd
tony-ngok <tonyn1999@outlook.com>
2024-10-30 16:22:00 -0500

    cosme: 更新代码

--- cosme/spiders/product.py
+++ cosme/spiders/product.py
@@ -3,14 +3,13 @@ import re
 
 import scrapy
 import scrapy.selector
+from scrapy.http import HtmlResponse
 from bs4 import BeautifulSoup
 
-# from resources.base_spider import BaseSpider
 
-
-class ProductSpider(BaseSpider):
+class ProductSpider(scrapy.Spider):
     name = "product"
-    allowed_domains = ["cosme.com"]
+    allowed_domains = ["www.cosme.com"]
     start_urls = []
 
     custom_settings = {
@@ -19,49 +18,31 @@ class ProductSpider(BaseSpider):
         }
     }
 
-    def get_headers(self):
-        headers = {
-            "Accept": "application/json, text/javascript, */*; q=0.01",
-            "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-            "Cache-Control": "no-cache",
-            "Connection": "keep-alive",
-            "Pragma": "no-cache",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-origin",
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-            "X-Requested-With": "XMLHttpRequest",
-            "sec-ch-ua": '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Linux"',
-        }
-
-        return headers
+    HEADERS = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "de-DE,de;q=0.9,en-GB;q=0.8,en;q=0.7",
+        "dnt": "1",
+        "priority": "u=0, i",
+        "sec-ch-ua": '"Chromium";v="130", "Microsoft Edge";v="130", "Not?A_Brand";v="99"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0"
+    }
 
     def __init__(self, *args, **kwargs):
-        super(ProductSpider, self).__init__(*args, **kwargs)
-        self.cookies = {
-            "PHPSESSID": "fhhf2q1g45dmm7jlt75pua8p35",
-            "@COSME_VISITOR": "VISITOR_ID=59d16095fc626471c6d07efd0ca5eb327aee27da",
-            "krt.vis": "jb9AOAAl3SujUGq",
-            "TMPPPTK": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ3bHQiOiJhZjZjMzQzMjQ0NjE2ZjBhZDhjNGNlOGMzYWE4MjY1ZSIsImNvcnMiOjEsIm5iZiI6MTcyMDcyMzI4MiwiZXhwIjoxNzUyMjU5MjgyfQ._UNgBKTyH9otOclxScACP2A5AKW23riiEYrm0D6xTXU",
-            "PPTK": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ3bHQiOiJhZjZjMzQzMjQ0NjE2ZjBhZDhjNGNlOGMzYWE4MjY1ZSIsImNvcnMiOjEsIm5iZiI6MTcyMDcyMzI4MiwiZXhwIjoxNzUyMjU5MjgyfQ._UNgBKTyH9otOclxScACP2A5AKW23riiEYrm0D6xTXU",
-            "__sna_s1d": "kXDqtp2TPGg3zQxwR2DzvGFfgNYKLh",
-            "_gid": "GA1.2.208641811.1721140007",
-            "product_history": "%5B%22331482%22%2C%22331910%22%2C%22323866%22%2C%22323864%22%5D",
-            "_gat": "1",
-            "_gat_commonTracker": "1",
-            "GA_LINKER_PARAM": "_ga%3D2.130410764.208641811.1721140007-1385196681.1720723281",
-            "_ga_HEKY12CWNG": "GS1.1.1721156002.6.0.1721156002.60.0.0",
-            "_ga": "GA1.1.1385196681.1720723281",
-            "cto_bundle": "n1QZW19CMnJjcVVFMVhBeGtWTkdJY21qOFklMkJtenNka0ZWYnVIZ3F5cFR6UmElMkIlMkJPWllQYzhPJTJCYVFCZXRSMzhZQXZyRlB3dmVHQ3pRdEpRODRLMnNzM0NzRWpVOTNHaWloYmYzdHdUZWZkR3laNzYxSUZ3TUNVJTJGdEZDODBUQTF0UmtMbFcxd2EzayUyRnBTJTJCbEx6Q29JYzBzMXZBJTJCUjRGZnBaZVN1VHNZQW5jNnZrd0RMYmdrS0hMRnBDa3VDNXRXSnZuUXo4MTJlUDR6TGFPN1BDcHJDZ1diT293USUzRCUzRA",
-        }
+        super().__init__(*args, **kwargs)
+        self.jpy_rate = 153.346419
 
-    def extract_pro_id_from_url(self, url):
-        pro_id = url.split("?product_id=")[1]
-        if "?" in pro_id:
-            pro_id = pro_id.split("?")[0]
-        return pro_id
+    def get_prod_id(self, url: str):
+        pid_match = re.findall(r'product_id=(\d+)', url)
+        if pid_match:
+            return pid_match[0]
 
     def remove_a_tag(self, text):
         soup = BeautifulSoup(text, "html.parser")
@@ -83,103 +64,30 @@ class ProductSpider(BaseSpider):
                 match.replaceWithChildren()
         return soup.text
 
-    def write_html_response_for_debug(self, url, text):
-        outHtmlFile = "output_product_detail_" + self.extract_pro_id_from_url(url) + ".html"
-        outHtmlFile = outHtmlFile.replace("/", "_")
-        outfileResponse = open("out_html/" + outHtmlFile, "w", encoding="utf-8")
-        outfileResponse.write(text)
-        outfileResponse.close()
-
     def start_requests(self):
-        for url in ProductSpider.start_urls:
-            yield scrapy.Request(
-                url,
-                method="GET",
-                headers=self.get_headers(),
-                cookies=self.cookies,
-                callback=self.parse_product,
-            )
+        for i, url in enumerate(self.start_urls):
+            yield scrapy.Request(url, headers=self.HEADERS,
+                                 meta={ "cookiejar": i },
+                                 callback=self.parse)
 
-    def get_usd_from_jyp(self, i_jyp):
-        return int((float(i_jyp) * 0.0063) * 100) / 100.0
+    def get_exist(self, response: HtmlResponse):
+        stockout_sel = response.css('.stockout__copy')
+        return not stockout_sel
 
-    def get_title(self, selector):
-        return selector.xpath("//p[@class='product__lead']/text()").get()
+    def get_title(self, response: HtmlResponse):
+        return response.xpath("//p[@class='product__lead']/text()").get()
 
-    def get_price(self, selector):
-        jyp_price = selector.xpath("//span[@itemprop='price']/@content").get()
+    def get_price(self, response: HtmlResponse):
+        jpy_price = response.xpath("//span[@itemprop='price']/@content").get()
+        return round(jpy_price*self.jpy_rate, 2)
 
-        us_dollar_price = self.get_usd_from_jyp(jyp_price)
-        return us_dollar_price
-
-    def get_brand(self, selector):
-        return selector.xpath("//p[@class='product__brand flt']//span/text()").get()
-
-    def get_categories(self, selector):
-        list_categories = selector.xpath(
-            "//div[@class='breadcrumb-wrap']/ul[@class='breadcrumb']/li//span/text()"
-        ).getall()
-        if list_categories[0] == "TOP":
-            list_categories.pop(0)
-
-        str_categories = " > ".join(list_categories)
-        return str_categories
-
-    def extractDigitListFromStr(self, string):
-        values = re.findall(r"[-+]?(?:\d*)", string)
-
-        rets = []
-
-        for x in values:
-            if x.isdigit():
-                rets.append(int(x))
-
-        return rets
-
-    def get_shipping_days(self, selector):
-        list_ship_days = []
-
-        product_condition = selector.xpath("//ul[@class='product-condition-list']").get()
-
-        min_days = 0
-        max_days = 0
-
-        if product_condition:
-            if "当日発送" in product_condition:
-                min_days = 1
-                max_days = 5
-            else:
-                min_days = 2
-                max_days = 5
-        else:
-            status = selector.xpath(
-                "//div[@class='wrap-product__stock']/p[@class='product__stock']"
-            ).get()
-
-            if "在庫あり" in status or "在庫残りわずか" in status:
-                min_days = 2
-                max_days = 5
-            else:
-                digits_list = self.extractDigitListFromStr(status)
-                if len(digits_list) == 2:
-                    min_days = digits_list[0]
-                    max_days = digits_list[1]
-                else:
-                    min_days = 7
-                    max_days = 10
-
-        list_ship_days.extend([min_days, max_days])
-
-        return list_ship_days
-
-    def get_shipping_fee(self, selector):
-        shipping_fee = 0
-        condition_list = selector.xpath("//ul[@class='product-condition-list']/li/text()").getall()
+    def get_shipping_fee(self, response: HtmlResponse):
+        condition_list = response.xpath("//ul[@class='product-condition-list']/li/text()").getall()
 
         if "送料無料" in condition_list:
-            shipping_fee = 0
+            shipping_fee = 0.00
         else:
-            shipping_fee = self.get_usd_from_jyp(319)
+            shipping_fee = round(319*self.jpy_rate, 2)
 
         return shipping_fee
 
@@ -224,35 +132,53 @@ class ProductSpider(BaseSpider):
 
         return desc
 
-    def get_specifications(self, selector, response):
-        specs = None
+    def get_specifications_etc(self, response: HtmlResponse):
+        specs = []
+        brand = None
+        cats = None
+        weight = None
 
-        dt_list = selector.xpath("//dl[@class='product-desc']/dt").getall()
-        dd_list = selector.xpath("//dl[@class='product-desc']/dd").getall()
+        thx = response.css('dl.product-desc > dt::text').getall()
+        tdx = response.css('dl.product-desc > dd')
 
-        if len(dt_list) != len(dd_list):  # should be same length
-            breakpoint()
+        for th, td in zip(thx, tdx):
+            if th and ('コスメ' not in th):
+                if th == 'ブランド名':
+                    brand_txt = td.css('a::text').get()
+                    if brand_txt:
+                        brand = brand_txt
+                elif th == 'アイテムカテゴリ':
+                    cats_txts = td.css(':scope a::text').getall()
+                    if cats_txts:
+                        cats = " > ".join(cats_txts)
+                elif td:
+                    specs.append({
+                        "name": th,
+                        "value": td,
+                    })
+                    if th == 'サイズ':
+                        weight = self.get_weight(td)
 
-        if len(dt_list) == len(dd_list):
-            specs = []
+        return (specs if specs else None), brand, cats, weight
 
-            for i, dt in enumerate(dt_list):
-                conv_dt = self.remove_a_tag_replace_content(dt_list[i])
-                conv_dt = conv_dt.replace("<dt>", "").replace("</dt>", "")
+    def get_weight(self, txt: str):
+        weight_match = re.findall(r'(\d+(?:\.\d+)?)\s*(g|ml|kg|l)', txt)
+        if weight_match:
+            if weight_match[1] in {'g', 'ml'}:
+                return round(float(weight_match[0])/453.59237, 2)
+            elif weight_match[1] in {'kg', 'l'}:
+                return round(float(weight_match[0])*2.20462, 2)
 
-                conv_dd = self.remove_a_tag_replace_content(dd_list[i])
-                conv_dd = conv_dd.replace("<dd>", "").replace("</dd>", "")
+    def get_reviews_num(self, response: HtmlResponse):
+        src_reviews = response.css('div.unique-product-rate-count::text').get()
+        if not src_reviews:
+            return
+        rev_match = re.findall(r'(\d+)', src_reviews)
+        if rev_match:
+            return int(rev_match[0])
 
-                specs.append({"name": conv_dt, "value": conv_dd})
-
-        return specs
-
-    def get_reviews_num(self, selector):
-        reviews_num = 0
-        return reviews_num
-
-    def get_rating(self, selector):
-        src_rating = selector.xpath("//span[@class='unique-product-rating-point']/text()").get()
+    def get_rating(self, response: HtmlResponse):
+        src_rating = response.xpath("//span[@class='unique-product-rating-point']/text()").get()
         if not src_rating:
             return
         f_src_rating = float(src_rating)
@@ -260,53 +186,44 @@ class ProductSpider(BaseSpider):
         conv_rating = (int(((f_src_rating / 7.0) * 5.0) * 10)) / 10
         return conv_rating
 
-    def parse_product(self, response):
-        self.update_cookies(response)
+    def parse(self, response: HtmlResponse):
+        existence = self.get_exist(response)
+        specifications, brand, categories, weight = self.get_specifications_etc(response)
 
-        item = ProductItem()
-        item.update(
-            {
-                "date": datetime.now().replace(microsecond=0).isoformat(),
-                "product_id": self.extract_pro_id_from_url(response.url),
-                "url": response.url,
-                "source": "cosme",
-                "returnable": False,
-            }
-        )
-        if response.status == 200:
-            item["existence"] = True
-        elif "ご指定のページが見つかりません" in response.text:
-            item["existence"] = False
-            yield item
-        selector = scrapy.Selector(text=response.text)
-        item["title"] = self.get_title(selector)
-        item["title_en"] = None
-        item["price"] = self.get_price(selector)
-        item["brand"] = self.get_brand(selector)
-        item["categories"] = self.get_categories(selector)
-        list_ship_days = self.get_shipping_days(selector)
-        item["shipping_days_min"] = list_ship_days[0]
-        item["shipping_days_max"] = list_ship_days[1]
-        item["shipping_fee"] = self.get_shipping_fee(selector)
-        item["reviews"] = self.get_reviews(selector)
-        item["rating"] = self.get_rating(selector)
-        item["images"] = self.get_images(selector)
-        item["sku"] = self.get_sku(selector)
-        item["description"] = self.get_description(selector)
-        item["description_en"] = None
-        item["specifications"] = self.get_specifications(selector, response)
-        item["summary"] = "{}, {}".format(item["title"], item["brand"])
-        item["upc"] = None
-        item["videos"] = None
-        item["available_qty"] = None
-        item["sold_count"] = None
-        item["weight"] = None
-        item["width"] = None
-        item["height"] = None
-        item["length"] = None
-
-        item["has_only_default_variant"] = True
-        item["variants"] = None
-        item["options"] = None
+        item = {
+            "date": datetime().strftime('%Y-%m-%dT%H:%M:%S'),
+            "url": response.url,
+            "source": "cosme",
+            "product_id": self.get_prod_id(response.url),
+            "existence": existence,
+            "title": self.get_title(response),
+            "title_en": None,
+            "description": self.get_description(response),
+            "description_en": None,
+            "summary": None,
+            "sku": self.get_sku(response),
+            "upc": self.get_sku(response),
+            "brand": brand,
+            "specifications": specifications,
+            "categories": categories,
+            "images": self.get_images(response),
+            "videos": None,
+            "price": price,
+            "available_qty": 0 if not existence else None,
+            "options": None,
+            "variants": None,
+            "has_only_default_variant": True,
+            "returnable": False,
+            "reviews": self.get_reviews(response),
+            "rating": self.get_rating(response),
+            "sold_count": None,
+            "shipping_fee": self.get_shipping_fee(response),
+            "shipping_days_min": 1, # https://www.cosme.com/help/help_04_2.html#yu_packet
+            "shipping_days_max": 2,
+            "weight": weight,
+            "length": None,
+            "width": None,
+            "height": None
+        }
 
         yield item
